6c217096e861f324cac96910314fa000c34541b7
Matthew Read <M.Read1@elsevier.com>
2021-12-08 16:28:57 +0000

    12 - data types + assignment

--- section-02/script.js
+++ section-02/script.js
@@ -1,13 +1,19 @@
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
-let firstName = "Matthew";
+let firstName = 'Matthew';
 console.log(firstName);
 
-let country = "England";
-let continent = "Europe";
+let country = 'England';
+let continent = 'Europe';
 let population = 68;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = true;
+let language;
+
+console.log(isIsland);
+console.log(language);
