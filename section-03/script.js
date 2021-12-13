48b9ee9b51f7e7920214a1dd5f58c768b6e0d28d
Matthew Read <m.read1@elsevier.com>
2021-12-13 15:42:31 +0000

    32 - strict mode

---
+++ section-03/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) haveDriversLicense = true; // strict mode will show an error in the console when running the programme
+if (hasDriversLicense) console.log('I can drive!');
+
+const interface = "Audio"; // will throw "Uncaught SyntxError: Unexpected strict mode reserved word"
+const private = "shh"; // as above
+const if = "if"; // as above
