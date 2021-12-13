113f4c15398e29be9cc2118f6c293e9588adf398
Matthew Read <m.read1@elsevier.com>
2021-12-13 15:59:52 +0000

    33 - functions

--- section-03/script.js
+++ section-03/script.js
@@ -1,4 +1,6 @@
 'use strict';
+/*
+// Strict mode
 
 let hasDriversLicense = false;
 const passTest = true;
@@ -8,4 +10,33 @@ if (hasDriversLicense) console.log('I can drive!');
 
 const interface = "Audio"; // will throw "Uncaught SyntxError: Unexpected strict mode reserved word"
 const private = "shh"; // as above
-const if = "if"; // as above
+const if = "if"; // as above */
+
+// Functions
+
+function logger() {
+    console.log('My name is Matthew')
+}
+
+logger();
+logger();
+logger(); // will log 'My name is Matthew' three times
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+function describeCountry(country, population, capitalCity) {
+    console.log(`${country} has ${population} million people and its capital city is ${capitalCity}`);
+}
+
+const describeFinland = describeCountry("Finland", 5.531, "Helsinki");
+const describeScotland = describeCountry("Scotland", 5.45, "Edinburgh");
+const describeItaly = describeCountry("Italy", 59.55, "Rome");
