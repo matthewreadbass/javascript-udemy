0ec1624c844ac7f0d7c08bb332818965357aea9b
Matthew Read <m.read1@elsevier.com>
2021-12-20 11:47:28 +0000

    59 - calculating temperature amplitude challenge #01

--- section-05/script.js
+++ section-05/script.js
@@ -1,4 +1,23 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const x = 4;
+// Calculate the amplitude of an array of temperatures - the thermometer will ocassionally throw an error
+
+const temperatures = [3, 15, -1, "error", 8, 8, 7, "error", 4];
+const temperatures2 = [6, 4, 8, 9, 0, "error"];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (typeof currentTemp !== "number") continue;
+
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
