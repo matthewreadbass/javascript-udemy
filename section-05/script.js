58f33695a805d9280e92bb7dc71c9c2dc7aeb274
Matthew Read <m.read1@elsevier.com>
2021-12-20 11:50:09 +0000

    59 - temperature amplitude calculator #02

--- section-05/script.js
+++ section-05/script.js
@@ -3,10 +3,33 @@
 
 // Calculate the amplitude of an array of temperatures - the thermometer will ocassionally throw an error
 
-const temperatures = [3, 15, -1, "error", 8, 8, 7, "error", 4];
+/* const temperatures = [3, 15, -1, "error", 8, 8, 7, "error", 4];
 const temperatures2 = [6, 4, 8, 9, 0, "error"];
 
 const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (typeof currentTemp !== "number") continue;
+
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude); */
+
+// Calculate the amplitude of an array of temperatures - the thermometer will ocassionally throw an error
+//     Function should be able to accept multiple arrays
+
+const temperatures = [3, 15, -1, "error", 8, 8, 7, "error", 4];
+const temperatures2 = [6, 4, 8, 9, 0, "error"];
+
+const calcTempAmplitude = function (t1, t2) {
+  let temps = t1.concat(t2);
   let max = temps[0];
   let min = temps[0];
 
