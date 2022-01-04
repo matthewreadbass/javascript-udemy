7ba58e33657922641f2b4930e474ae1d9e079fdc
Matthew Read <m.read1@elsevier.com>
2022-01-04 11:42:28 +0000

    72 - selecting and manipulating elements

--- section-07/script.js
+++ section-07/script.js
@@ -1,3 +1,9 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent);
+
+// changing values of classes from index.html
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
