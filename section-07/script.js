13d500c3a5e37c23a64df246171d4f488e2ae959
Matthew Read <m.read1@elsevier.com>
2022-01-04 14:44:20 +0000

    73 - handling click events

--- section-07/script.js
+++ section-07/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 // changing values of classes from index.html
@@ -7,3 +8,15 @@ document.querySelector('.message').textContent = 'Correct Number!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value; // saves the guessed number to the value 'guess'
+  console.log(guess); // logs guess to the console
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+    // if no number given, guess evaluates to falsey
+    // therefore !guess evaluates to truthy & the block will run
+  }
+});
