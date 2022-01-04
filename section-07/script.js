adcca704d9155aa38a031609ef9b6266d1e7e749
Matthew Read <m.read1@elsevier.com>
2022-01-04 15:08:25 +0000

    74 - implementing the game logic

--- section-07/script.js
+++ section-07/script.js
@@ -10,13 +10,39 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = document.querySelector('.guess').value; // saves the guessed number to the value 'guess'
+  const guess = Number(document.querySelector('.guess').value); // saves the guessed number to the value 'guess'
   console.log(guess); // logs guess to the console
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
     // if no number given, guess evaluates to falsey
     // therefore !guess evaluates to truthy & the block will run
+  } else if (guess === secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Correct Number!';
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
   }
 });
