47da1f86bf2a5383a33925db3419a1026373efc6
Matthew Read <m.read1@elsevier.com>
2022-01-05 11:26:18 +0000

    tidy up

--- section-07/script.js
+++ section-07/script.js
@@ -65,11 +65,14 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.score').textContent = '20';
+  score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.score').textContent = '20';
   document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.number').style.width = '15rem';
   document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
