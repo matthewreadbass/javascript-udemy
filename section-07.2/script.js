2bbc2dd1d7c88517aca52237d78dbfe3ceb6278c
Matthew Read <m.read1@elsevier.com>
2022-01-06 16:03:50 +0000

    80 - working with classes

--- section-07.2/script.js
+++ section-07.2/script.js
@@ -6,6 +6,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsShowModal.length; i++) {
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
