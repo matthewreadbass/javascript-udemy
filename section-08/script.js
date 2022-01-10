574c0ec0892a9c6fae2ca7aef30edb65d10c37b5
Matthew Read <m.read1@elsevier.com>
2022-01-10 14:10:36 +0000

    95 - hoisting and tdz in practice

--- section-08/script.js
+++ section-08/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function calcAge(birthYear) {
+/* function calcAge(birthYear) {
   const age = 2022 - birthYear;
   function printAge() {
     const output = `${firstName}, you are ${age}, born in ${birthYear}`;
@@ -14,7 +14,6 @@ function calcAge(birthYear) {
       function add(a, b) {
         return a + b;
       }
-      console.log(add());
     }
   }
   // console.log(millenial) => will work, as only let/const are block scoped, not var
@@ -22,7 +21,45 @@ function calcAge(birthYear) {
   return age;
 }
 
+    // Hoisting and TDZ in practice
+
 const firstName = 'Matthew';
 calcAge(1994);
 const a = 72;
-let b = 83;
+let b = 83; */
+
+console.log(me); // undefined - Variables declared with var are hoisted as undefined until they are declared later
+console.log(job); // cannot access let variables before they are declared (TDZ)
+console.log(birthYear); // cannot access const variables before they are declared (TDZ)
+
+var me = 'Matthew';
+let job = 'Developer';
+const birthYear = 1994;
+
+console.log(addDecl(2, 3)); // Will output 5 - function calls are hoisted
+console.log(addExpr(2, 3)); // Cannot access const variables before they are declared (TDZ)
+console.log(addArrow(2, 3)); // addArrow is not a function (is undefined before line being defined)
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts); // will log undefined
+if (!numProducts) deleteShoppingCart; // will delete shopping cart even though numProducts has been declared below as 10
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1; // will create a property on the window object
+let y = 2; // will not create a property on the window object
+const z = 3; // will not create a property on the window object
