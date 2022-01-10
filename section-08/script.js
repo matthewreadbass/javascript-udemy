6ddc9a2252205498b7350cc9514cbb068e47fa88
Matthew Read <m.read1@elsevier.com>
2022-01-10 15:20:43 +0000

    97 - the this keyword in practice

--- section-08/script.js
+++ section-08/script.js
@@ -26,7 +26,7 @@
 const firstName = 'Matthew';
 calcAge(1994);
 const a = 72;
-let b = 83; */
+let b = 83;
 
 console.log(me); // undefined - Variables declared with var are hoisted as undefined until they are declared later
 console.log(job); // cannot access let variables before they are declared (TDZ)
@@ -62,4 +62,38 @@ function deleteShoppingCart() {
 
 var x = 1; // will create a property on the window object
 let y = 2; // will not create a property on the window object
-const z = 3; // will not create a property on the window object
+const z = 3; // will not create a property on the window object  */
+
+// The this Keywords
+
+console.log(this); // points to the window object
+
+const calcAge = function (birthYear) {
+  console.log(2022 - birthYear);
+  console.log(this);
+};
+calcAge(1994); // logs undefined (in strict mode -> the global object in sloppy mode)
+
+const calcAgeArrow = birthYear => {
+  console.log(2022 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1994); // logs the window object - the arrow function does not get its own arrow function
+// Instead it uses the this keyword of its parents scope (window in this case)
+
+const matthew = {
+  year: 1994,
+  calcAge: function () {
+    console.log(2021 - this.year);
+  },
+};
+matthew.calcAge(); // logs the 'matthew' object
+// you can therefore use the this keyword inside the object to access other values inside the object
+
+const matilda = {
+  year: 2006,
+};
+
+matilda.calcAge = matthew.calcAge; // matilda object borrows the method from the matthew object
+matilda.calcAge(); // this now points to matilda
+// the this in the matthew object will still point to the matilda object if called by matilda
