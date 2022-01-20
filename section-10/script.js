aace39bac68c58f5ab9ed8b39d70a2080d0863b4
readm1 <M.Read1@elsevier.com>
2022-01-20 14:05:04 +0000

    129 - how passing arguments works: value vs reference

--- section-10/script.js
+++ section-10/script.js
@@ -3,7 +3,7 @@
 /*
 A Closer Look At Functions
 
-                // Default Parameters*/
+                // Default Parameters
 
 const bookings = [];
 
@@ -23,3 +23,43 @@ const createBooking = function (
 
 createBooking('LH125'); // {flightNum: 'LH125', numPassengers: 1, price: 80)}
 createBooking('LH125', 2, 800); // {flightNum: 'LH125', numPassengers: 2, price: 800)}
+
+                // How Passing Arguments Works: Value vs. Reference */
+
+const flight = 'LH234';
+const matthew = {
+  name: 'Matthew Read',
+  passport: 1234567890,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 1234567890) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, matthew);
+console.log(flight); // 'LH234'
+console.log(matthew); // 'Mr. Matthew Read'
+// one has changed, the other hasn't
+// flight is a primitive datatype (string), so flightNum (line 34) is a copy of line 29,
+// rather than a reference to it - line 35 flightNum is therefore a completely different
+// object in memory to line 29
+// all references to the matthew object (passenger/passenger.name/passenger.passport)
+// point to the same object in the heap because it is a complex datatype
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+newPassport(matthew); // changing passport number
+checkIn(flight, matthew); // checking in - passport number has changed (complex data type)
+// so newPassport has changed the original matthew object
+
+// Passing by Value - available in JavaScript
+// pass the value itself
+// Passing by Reference - not available in JavaScript
+// pass a reference to any value, rather than the value itself, even with primitives
