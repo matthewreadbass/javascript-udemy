8442314612401174d3385e25082ac5f44882bbf3
readm1 <M.Read1@elsevier.com>
2022-01-20 13:45:58 +0000

    128 - default parameters

--- section-10/script.js
+++ section-10/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+/*
+A Closer Look At Functions
+
+                // Default Parameters*/
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 80 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH125'); // {flightNum: 'LH125', numPassengers: 1, price: 80)}
+createBooking('LH125', 2, 800); // {flightNum: 'LH125', numPassengers: 2, price: 800)}
