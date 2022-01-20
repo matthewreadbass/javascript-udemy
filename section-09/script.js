e1d72e8f2913c70733083873e6d513908639da15
readm1 <M.Read1@elsevier.com>
2022-01-20 10:50:23 +0000

    125 - string methods practice

--- section-09/script.js
+++ section-09/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -941,9 +937,9 @@ const planesInLine = function (n) {
 };
 planesInLine(5);
 planesInLine(3);
-planesInLine(12); */
+planesInLine(12);
 
-// Coding Challenge #4
+        // Coding Challenge #4
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
@@ -958,4 +954,33 @@ document.querySelector('button').addEventListener('click', function () {
     const output = `${camelCase.padEnd(20, ' ')}${'✔'.repeat(i + 1)}`;
     console.log(output);
   }
-});
+});  */
+
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from/FAO to/LIS/(12h30)
+
+const flightDecoder = function (message) {
+  const flightsArr = flights.split('+');
+  console.log(flightsArr);
+  for (const flight of flightsArr) {
+    let [one, two, three, four] = flight.split(';');
+    one = `${one.slice(1).replace('_', ' ')} from`;
+    two = `${two.slice(0, 3).toUpperCase()} to`;
+    three = `${three.slice(0, 3).toUpperCase()}`;
+    let string = `${one} ${two} ${three} (${four})`;
+    if (string.startsWith('Delayed')) {
+      string = `🔴 ${string}`;
+      console.log(string.padStart(45, ' '));
+    } else {
+      console.log(string.padStart(45, ' '));
+    }
+  }
+};
+flightDecoder(flights);
